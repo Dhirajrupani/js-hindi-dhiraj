8dfa61c8ed9ba11c03f32f3db914e702b447c574
Dhirajrupani <134063439+Dhirajrupani@users.noreply.github.com>
2023-10-09 08:05:19 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "dhirajrupani4@gmail.com"
+var accountPassword ="12345"
+ accountCity="Ayodhya"
+  let accountState;
+
+ //accountId=123
+ console.log(accountId);
+ accountEmail="hs"
+ accountPassword="2121"
+ accountCity="fzd"
+ console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//  treat all js code as newer version
+
+// alert(3+3) // we are using notejs not browser
+
+console.log(3+
+    3)// code readability should be high
+
+    console.log("dhiraj")
+
+    let name="dhiraj"
+    //let age= 14
+    let age= null
+    let isLoggedIn = false
+
+    //number = 2 to power 53
+    //bigint 
+    // String == " "
+    //boolean == true or false
+    //null is a standalone value ( representation of a empty value)
+    // undefined ==> declare but not defined
+    // symbol ==> unique
+
+
+    // object
+
+
+    console.log(typeof "dhiraj");
+    console.log(typeof null);// as null is an object 
+    console.log(typeof undefined);// undefined is an datatype so it prints the same
