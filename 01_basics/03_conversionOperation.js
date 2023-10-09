fc27217939fd78cc63babab1156b0eb63bed82ee
Dhirajrupani <134063439+Dhirajrupani@users.noreply.github.com>
2023-10-09 10:24:46 +0000

    operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score =null
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"==> 33
+//"33abc"==>NaN
+// true =1 and false =0
+
+let isBoolean = true
+let id=Number(isBoolean)
+console.log(id);
+console.log(typeof id);
+let money=567
+let isMoney =String(money)
+console.log(isMoney);
+console.log(typeof isMoney);
